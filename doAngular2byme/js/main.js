c5f695d4487bda2873af85d49ede5251b1da8f6f
EahanZhang <490742241@qq.com>
2016-07-09 15:58:25 +0800

    simple test of databind

--- doAngular2byme/js/main.js
+++ doAngular2byme/js/main.js
@@ -7,23 +7,8 @@ app.run(function($rootScope) {
 	$rootScope.name = "Allen Iverson";
 });
 
-app.controller("PlayerController", ['$scope', function($scope) {
-	$scope.playing = false;
-	$scope.audio = document.createElement('audio');
-	$scope.audio.src = "/home/yhzhang/media/music.mp3"
-	$scope.play = function() {
-		$scope.audio.play();
-		$scope.playing = true;
-	};
-	$scope.stop = function() {
-		$scope.audio.pause();
-		$scope.playing = false;
-	};
-	$scope.audio.addEventListener('ended', function() {
-		$scope.apply(function() {
-			$scope.stop()
-		});
-	});
+app.controller("MyController1", ['$scope', function($scope) {
+
 }]);
 
 
@@ -31,3 +16,19 @@ app.controller("RelatedController", ['$scope', function($scope) {
 	
 }]);
 
+
+app.controller("MyController", function($scope) {
+	$scope.person = {
+		name: "Allen Iverson"
+	};
+	$scope.count = 0;
+	var updateClock = function() {
+		$scope.clock = new Date();
+	};
+
+	var timer = setInterval(function() {
+		$scope.$apply(updateClock);
+		$scope.count += 1;
+	}, 1000);
+	updateClock();
+});
