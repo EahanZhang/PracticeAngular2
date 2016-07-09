714d2555db4aeedc16a3bf0440b70f2d58ae1223
EahanZhang <490742241@qq.com>
2016-07-09 14:41:22 +0800

    using scope to change a property's value

--- doAngular2byme/js/main.js
+++ doAngular2byme/js/main.js
@@ -7,12 +7,27 @@ app.run(function($rootScope) {
 	$rootScope.name = "Allen Iverson";
 });
 
-app.controller('ParentController', function($scope) {
-	$scope.person = {greeted: false};
-});
+app.controller("PlayerController", ['$scope', function($scope) {
+	$scope.playing = false;
+	$scope.audio = document.createElement('audio');
+	$scope.audio.src = "/home/yhzhang/media/music.mp3"
+	$scope.play = function() {
+		$scope.audio.play();
+		$scope.playing = true;
+	};
+	$scope.stop = function() {
+		$scope.audio.pause();
+		$scope.playing = false;
+	};
+	$scope.audio.addEventListener('ended', function() {
+		$scope.apply(function() {
+			$scope.stop()
+		});
+	});
+}]);
 
-app.controller('ChildController', function($scope) {
-	$scope.sayhello = function() {
-		$scope.person.greeted = true;
-	}
-});
+
+app.controller("RelatedController", ['$scope', function($scope) {
+	
+}]);
+
